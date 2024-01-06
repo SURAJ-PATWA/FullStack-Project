66cc9eca9b74058e032e0a7ea908a983928fba0c
Suraj Patwa <surajpatwa1506@gmail.com>
2024-01-06 16:41:50 +0530

    change the path

--- frontend/my-pages/src/Login.js
+++ frontend/my-pages/src/Login.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import axios from "axios";
-
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
 
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        navigate('/home');
 
 
         setEmail("");
@@ -35,7 +37,7 @@ const Login = () => {
                 .then((res) => {
                     if (res.status === 200) {
                        
-                        window.location.href = "/home";
+                        // window.location.href = "/home";
                     } else {
                         
                         console.log(res.data.error);
